ea545f6c72b143c9a4d70cf1738e4adf53421bae
Anna Agozzino <anna.agozzino91@gmail.com>
2020-10-05 19:32:42 +0200

    Fix Snack 2 - eliminati controlli doppi su age

--- php-snack-2/index.php
+++ php-snack-2/index.php
@@ -13,21 +13,20 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
     <?php
       //Controlo che le variabili in GET non siano vuote e che age sia un numero
       if (!empty($_GET["name"]) && !empty($_GET["email"]) && !empty($_GET["age"] && is_numeric($_GET["age"])) ) {
-        // Setto le variabili e trasformo $age da stringa a intero
+        // Setto le variabili e trasformo $age da str
         $name = $_GET["name"];
         //var_dump($name);
         $mail = $_GET["email"];
         //var_dump($mail);
-        $age = intval($_GET["age"]);
-        //var_dump(gettype($age));
-        // Controlo che name>3
-        if (strlen($name)>3 && strpos($mail, ".")!== false && strpos($mail, "@") !== false && is_int($age)) {
-          echo "Accesso riuscito";
+
+        // Controlo che name>3 e che mail contenga un punto e una @
+        if (strlen($name)>3 && strpos($mail, ".")!== false && strpos($mail, "@") !== false) {
+          echo "<h1>Accesso riuscito</h1>";
         }else {
-          echo "Accesso negato";
+          echo "<h1>Accesso negato</h1>";
         }
       } else {
-        echo "Accesso negato";
+        echo "<h1>Accesso negato</h1>";
       };
 
     ?>
